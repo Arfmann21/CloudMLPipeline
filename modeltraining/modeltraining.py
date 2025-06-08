60c9f0becf1dfdcfc9cf738c435d45fe78df2255
Arfmann21 <arfmann@protonmail.com>
2025-06-08 15:38:54 +0200

    Changed from SVC to Extreme Gradient Boost to take advantage of the distributed system

--- modeltraining/modeltraining.py
+++ modeltraining/modeltraining.py
@@ -1,19 +1,29 @@
 import pandas as pd
 from sklearn.model_selection import train_test_split
-from sklearn.svm import SVC
+from sklearn.preprocessing import LabelEncoder
+
 import joblib
 import tempfile
 from google.cloud import storage
+import xgboost as xgb
+
 
 def main(request):
     df = pd.read_csv("gs://progetto_clean_dataset_bucket/dataset.csv")
 
     X = df.drop("class", axis=1)
     y = df["class"]
+
+    label_encoder = LabelEncoder()
+    y = label_encoder.fit_transform(y)
+
     X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.2, random_state=42)
 
-    # Modello SVC
-    model = SVC(kernel='linear')  # puoi provare anche 'rbf', 'poly'
+    model = xgb.XGBClassifier(
+        objective='multi:softprob',  # o 'binary:logistic' se hai solo 2 classi
+        eval_metric='mlogloss',  # o 'logloss' per binario
+        random_state=42
+    )
     model.fit(X_train, y_train)
 
     with tempfile.NamedTemporaryFile(suffix=".pkl", delete=False) as tmp:
