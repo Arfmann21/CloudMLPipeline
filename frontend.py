0b071a318cb20d54c902be987e105502c91da332
Arfmann21 <arfmann@protonmail.com>
2025-06-11 16:16:02 +0200

    Added Pub/Sub to listen for preprocessing status

--- frontend.py
+++ frontend.py
@@ -5,11 +5,21 @@ import url_secret as us
 import pandas as pd
 import ast
 import io
+from google.cloud import pubsub_v1
 
 st.set_page_config(layout = "wide")
 
+preprocessing_completed = False
+
+def sub_callback(message):
+    message.ack()
+    global preprocessing_completed
+    preprocessing_completed = True
+    st.write(preprocessing_completed)
+
 def load_to_bucket(dataset):
 
+    # Show a loading for uploading the dataset into the bucket
     with st.spinner("Preprocessing in corso..."):
         storage_client = storage.Client()
         bucket_name = "progetto_dataset_bucket"
@@ -20,6 +30,21 @@ def load_to_bucket(dataset):
 
         blob.upload_from_file(dataset)
 
+        # The frontend must know when the preprocessing is completed before doing anything
+        # This is achieved using Google Cloud Pub/Sub pattern
+        p_id = us.get_id()
+        s_id = us.get_sub_id()
+        global preprocessing_completed
+
+        subscriber = pubsub_v1.SubscriberClient()
+        subsctibtion_path = subscriber.subscription_path(p_id, s_id)
+        subscriber.subscribe(subsctibtion_path, callback = sub_callback)
+
+        # Do nothing until the preprocessing is completed
+        while not preprocessing_completed:
+            st.spinner()
+
+    # Once the preprocessing is done, start training the machine learning model
     start_training()
 
 def start_training():
@@ -38,13 +63,58 @@ def inference(inf_with_file, file, buying, maint, doors, persons, lug_boot, safe
     lug_boot = 0 if lug_boot == "small" else (1 if lug_boot == "med" else 2)
     safety = 0 if safety == "low" else (1 if safety == "med" else 2)
 
-    return requests.post(f"{us.get_inference()}?inf_with_file=False&buying={buying}&maint={maint}&doors={doors}&persons={persons}&lug_boot={lug_boot}&safety={safety}")
+    return requests.post(f"{us.get_inference()}?buying={buying}&maint={maint}&doors={doors}&persons={persons}&lug_boot={lug_boot}&safety={safety}")
 
+
+def form_inference_handler():
+    with st.form("inference_form"):
+        buying = st.selectbox("Buying", options=["low", "med", "high", "v-high"])
+        maint = st.selectbox("Maint", options=["low", "med", "high", "v-high"])
+        doors = st.selectbox("Doors", options=["2", "3", "4", "5-more"])
+        persons = st.selectbox("Persons", options=["2", "4", "more"])
+        lug_boot = st.selectbox("Lug Boot", options=["small", "med", "big"])
+        safety = st.selectbox("Safety", options=["low", "med", "high"])
+
+        submitted = st.form_submit_button("Classifica")
+
+        if submitted:
+            result = inference(False, None, buying, maint, doors, persons, lug_boot, safety)
+
+            if result is not None:
+                result = result.content.decode().strip()
+                result = "acc" if result == "0" else (
+                    "good" if result == "1" else ("unacc" if result == "2" else "vgood"))
+                st.subheader("Etichetta: " + result)
+
+
+def file_inference_handler():
+
+    dataset_inference_uploader = st.file_uploader(label="Seleziona file per l'inferenza", type="csv")
+
+    if dataset_inference_uploader is not None:
+        df = pd.read_csv(io.BytesIO(dataset_inference_uploader.getvalue()))
+        result = inference(True, dataset_inference_uploader, 0, 0, 0, 0, 0, 0)
+
+        result_classes = ast.literal_eval(result.content.decode())
+        result_classes_str = []
+
+        with st.spinner():
+
+            if result is not None:
+                for i in range(0, len(result_classes)):
+                    result_classes_str.append("acc" if result_classes[i] == 0 else (
+                        "good" if result_classes[i] == 1 else ("unacc" if result_classes[i] == 2 else "vgood")))
+
+                df["class"] = result_classes_str
+
+                st.dataframe(df)
 
 def main():
 
+    # Use session state to dinamically change the UI to show inference when the dataset is uploaded
     if "dataset_loaded" not in st.session_state:
         st.session_state["dataset_loaded"] = False
+        st.session_state["file_inf"] = False
         st.header("Benvenuto, inizia caricando il dataset")
 
     if not st.session_state["dataset_loaded"]:
@@ -53,47 +123,19 @@ def main():
         if dataset is not None:
             st.session_state["dataset_loaded"] = True
             load_to_bucket(dataset)
-            st.subheader("Modello pronto per l'inferenza")
             st.rerun()
 
     else:
-        result = None
         st.header("Inserisci i dati per classificare il record")
 
+        # Create three columns: the first one to display the form, the third to display the file uploader and the dataframe
+        # and the middle to add some space in between
         form_inference, or_text, file_inference = st.columns([1, 0.2, 2])
 
         with form_inference:
-            with st.form("inference_form"):
-                buying = st.selectbox("Buying", options = ["low", "med", "high", "v-high"])
-                maint = st.selectbox("Maint", options = ["low", "med", "high", "v-high"])
-                doors = st.selectbox("Doors", options = ["2", "3", "4", "5-more"])
-                persons = st.selectbox("Persons", options = ["2", "4", "more"])
-                lug_boot = st.selectbox("Lug Boot", options = ["small", "med", "big"])
-                safety = st.selectbox("Safety", options = ["low", "med", "high"])
-
-                submitted  = st.form_submit_button("Classifica")
-
-                if submitted:
-                    result = inference(False, None, buying, maint, doors, persons, lug_boot, safety)
-
-                    if result is not None:
-                        result = result.content.decode().strip()
-                        result = "acc" if result == "0" else (
-                            "good" if result == "1" else ("unacc" if result == "2" else "vgood"))
-                        st.subheader("Etichetta: " + result)
+            form_inference_handler()
 
         with file_inference:
-            dataset_inference_uploader = st.file_uploader(label = "Seleziona file per l'inferenza", type = "csv")
-
-            if dataset_inference_uploader is not None:
-                df = pd.read_csv(io.BytesIO(dataset_inference_uploader.getvalue()))
-                result = inference(True, dataset_inference_uploader, 0, 0, 0, 0, 0, 0)
-
-                if result is not None:
-                    df["class"] = ast.literal_eval(result.content.decode())
-
-                    st.dataframe(df)
-
-
+            file_inference_handler()
 
 main()
